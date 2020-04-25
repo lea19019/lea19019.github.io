caa3fbac9b843d425c5cffdc719434fbee0b5cbc
lea19019 <lea19019@byui.edu>
2020-04-25 09:04:45 -0500

    year2

--- javascript.js
+++ javascript.js
@@ -1,1 +1,5 @@
 document.getElementById('last').textContent = document.lastModified
+
+let options = { year: "numeric"};
+
+document.getElementById("currentYear").textContent = new Date().toLocaleDateString('en-US', options);
