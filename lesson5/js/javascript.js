92893ec9888258e2970cb28c2f0a75c3fc9bb587
lea19019 <lea19019@byui.edu>
2020-05-23 21:10:06 -0500

    changes

--- lesson5/js/javascript.js
+++ lesson5/js/javascript.js
@@ -10,3 +10,11 @@ const mainnav = document.querySelector('.navigation')
 
 barnavbutton.addEventListener('click', () =>
 {mainnav.classList.toggle('responsive')}, false);
+
+let show = new Date();
+
+if(show.getDay() == 6 ){
+    document.getElementById("pancakes").style.display = "block";
+} else{
+    document.getElementById("pancakes").style.display = "none";
+}
