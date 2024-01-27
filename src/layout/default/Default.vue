24a331a0f1594c217c098c05091bb47d17c8c46e
Adrian <vadriancastillo2@gmail.com>
2024-01-27 13:22:52 -0600

    working...

---
+++ src/layout/default/Default.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <VApp>
+    <RouterView />
+  </VApp>
+</template>
+
+<style scoped lang="scss">
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,8 @@
+<template>
+    <VCard>
+        <VCardTitle>Home</VCardTitle>
+        <VCardText>
+            <p>Home page</p>
+        </VCardText>
+    </VCard>
+</template>
